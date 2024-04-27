a902cb53f115e7d2a8c39f3d84de13068462e5a4
MaheshKummari <maheshkummari7111@gmail.com>
2024-04-27 12:50:56 +0530

    code formatted

--- routes/cartRouts.js
+++ routes/cartRouts.js
@@ -1,135 +1,135 @@
-import Cart from "../models/Cart.js";
-import Product from "../models/Product.js";
-import User from "../models/User.js";
-import { authenticateUser } from "./auth.js";
-import { router } from "./routes.js";
+import Cart from '../models/Cart.js';
+import Product from '../models/Product.js';
+import User from '../models/User.js';
+import { authenticateUser } from './auth.js';
+import { router } from './routes.js';
 
-router.get("/cart", authenticateUser, async (req, res) => {
-  console.log("in-cart");
-  const { email } = req.user;
+router.get('/cart', authenticateUser, async (req, res) => {
+	console.log('in-cart');
+	const { email } = req.user;
 
-  console.log(email);
-  try {
-    const user = await User.findOne({ email });
+	console.log(email);
+	try {
+		const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
 
-    const cartItems = await Cart.findOne({ email: user.email });
+		const cartItems = await Cart.findOne({ email: user.email });
 
-    if (!cartItems || cartItems?.length === 0) {
-      return res.status(404).json({ message: "Cart is empty", products: [] });
-    }
+		if (!cartItems || cartItems?.length === 0) {
+			return res.status(404).json({ message: 'Cart is empty', products: [] });
+		}
 
-    res.json(cartItems.items);
-  } catch (error) {
-    console.error(`Database query error: ${error.message}`);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+		res.json(cartItems.items);
+	} catch (error) {
+		console.error(`Database query error: ${error.message}`);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 });
 
-router.post("/addToCart", authenticateUser, async (req, res) => {
-  const { productId, quantity } = req.body;
-  const { email } = req.user;
-  console.log(email, quantity, productId);
+router.post('/addToCart', authenticateUser, async (req, res) => {
+	const { productId, quantity } = req.body;
+	const { email } = req.user;
+	console.log(email, quantity, productId);
 
-  try {
-    let cart = await Cart.findOne({ email: email });
-    let productDetails = await Product.findOne({
-      productId: req.body.productId,
-    });
-    const { productId, name, price, discounted_price, description, image } =
-      productDetails;
+	try {
+		let cart = await Cart.findOne({ email: email });
+		let productDetails = await Product.findOne({
+			productId: req.body.productId,
+		});
+		const { productId, name, price, discounted_price, description, image } =
+			productDetails;
 
-    console.log("cart", cart);
+		console.log('cart', cart);
 
-    if (!cart) {
-      cart = new Cart({
-        email: email,
-        items: [
-          {
-            productId,
-            name,
-            price,
-            discounted_price,
-            description,
-            image,
-            quantity,
-          },
-        ],
-      });
-    } else {
-      const itemIndex = cart.items.findIndex(
-        (item) => item.productId === productId
-      );
-      console.log("itemIndex", itemIndex);
+		if (!cart) {
+			cart = new Cart({
+				email: email,
+				items: [
+					{
+						productId,
+						name,
+						price,
+						discounted_price,
+						description,
+						image,
+						quantity,
+					},
+				],
+			});
+		} else {
+			const itemIndex = cart.items.findIndex(
+				(item) => item.productId === productId
+			);
+			console.log('itemIndex', itemIndex);
 
-      if (itemIndex !== -1) {
-        cart.items[itemIndex].quantity += quantity;
-      } else {
-        cart.items.push({
-          productId,
-          name,
-          price,
-          discounted_price,
-          description,
-          image,
-          quantity,
-        });
-      }
-    }
-    console.log("not saved yet", cart);
+			if (itemIndex !== -1) {
+				cart.items[itemIndex].quantity += quantity;
+			} else {
+				cart.items.push({
+					productId,
+					name,
+					price,
+					discounted_price,
+					description,
+					image,
+					quantity,
+				});
+			}
+		}
+		console.log('not saved yet', cart);
 
-    await cart.save();
+		await cart.save();
 
-    res
-      .status(200)
-      .json({ message: `${productDetails?.name} added to cart successfully!` });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error adding item to cart", error: error.message });
-  }
+		res
+			.status(200)
+			.json({ message: `${productDetails?.name} added to cart successfully!` });
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: 'Error adding item to cart', error: error.message });
+	}
 });
 
 router.delete(
-  "/deleteProductFromCart/:productId",
-  authenticateUser,
-  async (req, res) => {
-    let { productId } = req.params;
-    productId = parseInt(productId);
+	'/deleteProductFromCart/:productId',
+	authenticateUser,
+	async (req, res) => {
+		let { productId } = req.params;
+		productId = parseInt(productId);
 
-    const { email } = req.user;
+		const { email } = req.user;
 
-    try {
-      let cart = await Cart.findOne({ email: email });
-      // console.log('cart', cart);
+		try {
+			let cart = await Cart.findOne({ email: email });
+			// console.log('cart', cart);
 
-      if (!cart) {
-        res.json({ message: "Item not found" });
-      } else {
-        const itemIndex = cart.items.findIndex(
-          (item) => item.productId === productId
-        );
-        console.log("itemIndex", itemIndex);
+			if (!cart) {
+				res.json({ message: 'Item not found' });
+			} else {
+				const itemIndex = cart.items.findIndex(
+					(item) => item.productId === productId
+				);
+				console.log('itemIndex', itemIndex);
 
-        if (itemIndex === -1) {
-          res.json({ message: "Item not found" });
-        } else {
-          const deletedItem = cart.items.splice(itemIndex, 1);
-          await cart.save();
+				if (itemIndex === -1) {
+					res.json({ message: 'Item not found' });
+				} else {
+					const deletedItem = cart.items.splice(itemIndex, 1);
+					await cart.save();
 
-          res.status(200).json({
-            message: `${deletedItem[0]?.name} removed from cart successfully`,
-          });
-        }
-      }
-    } catch (error) {
-      res.status(500).json({
-        message: "Error removing item from cart",
-        error: error.message,
-      });
-    }
-  }
+					res.status(200).json({
+						message: `${deletedItem[0]?.name} removed from cart successfully`,
+					});
+				}
+			}
+		} catch (error) {
+			res.status(500).json({
+				message: 'Error removing item from cart',
+				error: error.message,
+			});
+		}
+	}
 );
--- routes/contactRouts.js
+++ routes/contactRouts.js
@@ -1,58 +1,58 @@
-import Contact from "../models/Contact.js";
+import Contact from '../models/Contact.js';
 // import { authenticateUser } from "./auth";
-import { router } from "./routes.js";
+import { router } from './routes.js';
 
-router.get("/contact/getMessages/:email", async (req, res) => {
-  const { email } = req.params;
-  console.log(email);
-  try {
-    const user = await Contact.findOne({ email: email });
-    console.log("user :", user);
-    if (user) {
-      console.log("in user");
-      const messages = user?.messages;
-      console.log("msgs", messages);
-      if (messages.length > 0) {
-        res.send({ message: "Messages found", messages: messages });
-      } else {
-        res.send({ message: "Messages not found", messages: [] });
-      }
-    } else {
-      res.send({ message: "user not found", messages: [] });
-    }
-  } catch (error) {
-    console.error(`Database query error: ${error.message}`);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+router.get('/contact/getMessages/:email', async (req, res) => {
+	const { email } = req.params;
+	console.log(email);
+	try {
+		const user = await Contact.findOne({ email: email });
+		console.log('user :', user);
+		if (user) {
+			console.log('in user');
+			const messages = user?.messages;
+			console.log('msgs', messages);
+			if (messages.length > 0) {
+				res.send({ message: 'Messages found', messages: messages });
+			} else {
+				res.send({ message: 'Messages not found', messages: [] });
+			}
+		} else {
+			res.send({ message: 'user not found', messages: [] });
+		}
+	} catch (error) {
+		console.error(`Database query error: ${error.message}`);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 });
 
-router.post("/contact/addMessage", async (req, res) => {
-  const { name, email, subject, message } = req.body;
-  console.log("req-body", req.body);
+router.post('/contact/addMessage', async (req, res) => {
+	const { name, email, subject, message } = req.body;
+	console.log('req-body', req.body);
 
-  try {
-    let contact = await Contact.findOne({ email });
-    console.log("checking-contact", contact);
-    if (!contact) {
-      contact = new Contact({
-        name,
-        email,
-        messages: [
-          {
-            subject,
-            message,
-          },
-        ],
-      });
-    } else {
-      contact.messages.push({ subject, message });
-    }
-    console.log("contact :", contact);
+	try {
+		let contact = await Contact.findOne({ email });
+		console.log('checking-contact', contact);
+		if (!contact) {
+			contact = new Contact({
+				name,
+				email,
+				messages: [
+					{
+						subject,
+						message,
+					},
+				],
+			});
+		} else {
+			contact.messages.push({ subject, message });
+		}
+		console.log('contact :', contact);
 
-    await contact.save();
-    res.status(200).json({ message: `${subject} added successfully!` });
-  } catch (error) {
-    console.error(`Database query error: ${error.message}`);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+		await contact.save();
+		res.status(200).json({ message: `${subject} added successfully!` });
+	} catch (error) {
+		console.error(`Database query error: ${error.message}`);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 });
