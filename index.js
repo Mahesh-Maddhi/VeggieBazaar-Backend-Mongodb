57b83185c7204151844f5b41dbddeb17eff652ff
MaheshKummari <maheshkummari7111@gmail.com>
2024-04-27 11:59:19 +0530

    updated contactRoutes.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import dontenv from 'dotenv';
-import cors from 'cors';
-import { router } from './routes/routes.js';
-import { connectToDatabase } from './config/database.js';
-import './routes/productRouts.js';
-import './routes/userRouts.js';
-import './routes/cartRouts.js'
+import express from "express";
+import dontenv from "dotenv";
+import cors from "cors";
+import { router } from "./routes/routes.js";
+import { connectToDatabase } from "./config/database.js";
+import "./routes/productRouts.js";
+import "./routes/userRouts.js";
+import "./routes/cartRouts.js";
+import "./routes/contactRouts.js";
 
 dontenv.config();
 
@@ -16,12 +17,12 @@ const PORT = process.env.PORT || 5000;
 connectToDatabase();
 
 app.listen(PORT, () => {
-	console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 app.use(express.json());
 app.use(cors());
-app.use('/', router);
+app.use("/", router);
 
 export { router };
 export default app;
