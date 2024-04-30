87a0295e755219f1963029c563f79444ef1fcd4d
Mahesh Maddhi <maddimahesh111@gmail.com>
2024-04-30 16:44:12 +0530

    minor changes done

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,7 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import { authenticateUser } from './auth.js';
 dotenv.config();
 const router = express.Router();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 router.get('/', (req, res) => {
 	try {
@@ -17,118 +12,5 @@ router.get('/', (req, res) => {
 		});
 	}
 });
-// router.get('/search', async (req, res) => {
-// 	const { q } = req.query;
-// 	const searchProductsQuery = `SELECT * FROM products WHERE name LIKE '%${q}%'
-//    OR description LIKE '%${q}%' ;`;
-// 	try {
-// 		let productsArray = await db.all(searchProductsQuery);
-// 		res.json({ products: productsArray });
-// 	} catch (error) {
-// 		res.status(500).json({
-// 			message: `Internal Server Error ${error.message} - search`,
-// 		});
-// 	}
-// });
-
-// router.get('/cart', authenticateUser, async (req, res) => {
-// 	console.log('in-cart');
-// 	const email = req.email;
-// 	try {
-// 		const data = await db.all(
-// 			`SELECT * FROM cart INNER JOIN products ON cart.product_id = products.id WHERE email LIKE "${email}"`
-// 		);
-// 		res.json(data);
-// 	} catch (error) {
-// 		console.error(`Database query error: ${error.message}`);
-// 		res.status(500).json({ message: 'Internal Server Error' });
-// 	}
-// });
-
-// router.post('/addProductToCart', authenticateUser, async (req, res) => {
-// 	let { productId, quantity } = req.body;
-// 	const email = req.email;
-
-// 	console.log('in add-to-cart', productId, email, quantity);
-
-// 	try {
-// 		const getProductIdQuery = `SELECT * FROM cart INNER JOIN users ON cart.email = users.email WHERE cart.product_id = ${productId} AND users.email LIKE "${email}";`;
-// 		let cartProduct = await db.get(getProductIdQuery);
-// 		console.log('cart-product', cartProduct);
-
-// 		if (cartProduct === undefined) {
-// 			let addProductToCartQuery = `INSERT INTO
-//                 cart("email", "product_id", "quantity")
-//                 values("${email}", ${productId}, ${quantity});`;
-// 			try {
-// 				await db.run(addProductToCartQuery);
-// 				res.json({
-// 					message: `Item added to cart successfully.`,
-// 				});
-// 			} catch (error) {
-// 				console.log(error);
-// 				res.status(500).json({
-// 					message: `Internal server error - add to cart : ${error.message}`,
-// 				});
-// 			}
-// 		} else {
-// 			let updateProductToCartQuery = `UPDATE cart
-//                 SET quantity =  ${quantity}
-//                  WHERE email LIKE "${email}"
-//                  AND product_id = ${productId};`;
-// 			try {
-// 				await db.run(updateProductToCartQuery);
-// 				res.json({
-// 					message: `${cartProduct.name} quantity updated to ${quantity}.`,
-// 				});
-// 			} catch (error) {
-// 				res
-// 					.status(500)
-// 					.json({ message: `Internal server error : ${error.message}` });
-// 			}
-// 		}
-// 	} catch (error) {
-// 		res.status(500).json({
-// 			message: `Internal server error -add to cart : ${error.message}`,
-// 		});
-// 	}
-// });
-// router.delete(
-// 	'/deleteProductFromCart/:productId',
-// 	authenticateUser,
-// 	async (req, res) => {
-// 		const { productId } = req.params;
-// 		const email = req.email;
-
-// 		try {
-// 			const getProductIdQuery = `SELECT * FROM cart
-//         WHERE product_id = ${productId}
-//         AND email LIKE "${email}";`;
-// 			let cartProduct = await db.get(getProductIdQuery);
-
-// 			if (cartProduct === undefined) {
-// 				res.status(404).json({ message: `product not found in cart` });
-// 			} else {
-// 				let deleteProductFromCartQuery = `DELETE FROM cart
-//                 WHERE email LIKE "${email}"
-//                 AND product_id = ${productId};`;
-// 				try {
-// 					await db.run(deleteProductFromCartQuery);
-// 					res.json({
-// 						message: `Item removed from cart Successfully.`,
-// 					});
-// 				} catch (error) {
-// 					res
-// 						.status(500)
-// 						.json({ message: `Internal server error : ${error.message}` });
-// 				}
-// 			}
-// 		} catch (error) {
-// 			res
-// 				.status(500)
-// 				.json({ message: `Internal server error : ${error.message}` });
-// 		}
-// 	}
-// );
 
 export { router };
