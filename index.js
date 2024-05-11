0f892680c6a84f760864d4c5aeec7d7b16142978
Mahesh Maddhi <maddimahesh111@gmail.com>
2024-05-11 19:05:27 +0530

    removed cookies

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ const corsOptions = {
 		'http://localhost:3000',
 		'http://localhost:5000',
 	],
-	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 };
